b29f28288e56d6f507efb44d871c4f297ce72753
Robert Horrocks <rhorrocks@bds.local>
2016-09-27 16:28:09 -0400

    first commit

---
+++ HorrocksPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HorrocksPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "How Robert helps organizations like yours.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Get a booster pack and two extra lives!!!";
+
+            return View();
+        }
+    }
+}
---
+++ HorrocksPortfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HorrocksPortfolio.MvcApplication" Language="C#" %>
